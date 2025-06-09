d52f25c517376306c8f9690fb3e515aaaaf8f931
BP15 <lawhitenton62@gmail.com>
2025-06-09 13:06:29 -0500

    Create PluginInfo.cs

---
+++ PluginInfo.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PCNeckMovement
+{
+    public class PluginInfo
+    {
+        public const string GUID = "com.BP15.PCNeckMovement";
+        public const string Name = "PCNeckMovement";
+        public const string Version = "1.0.0";
+    }
+}
