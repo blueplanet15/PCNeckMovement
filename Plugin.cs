a841d26d7763de9d6eb61b95c55770612fb963d3
BP15 <lawhitenton62@gmail.com>
2025-06-08 20:02:16 -0500

    Update Plugin.cs

--- Plugin.cs
+++ Plugin.cs
@@ -5,11 +5,7 @@ namespace PCNeckMovement
 {
     public class Plugin
     {
-        public void Awake()
-        {
-            GorillaTagger.OnPlayerSpawned(Initialize);
-        }
-        private async void Initialize()
+        void Start()
         {
             GorillaTagger.Instance.offlineVRRig.head.overrideTarget = GorillaTagger.Instance.mainCamera.transform;
         }
